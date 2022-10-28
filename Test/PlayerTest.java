35fbfad3d266930569ea7836b1eb86ce0511405d
1601230 <1601230@uab.cat>
2022-10-28 10:18:28 +0200

    COMMIT 1: Fase Red de Player

--- Test/PlayerTest.java
+++ Test/PlayerTest.java
@@ -1,5 +1,18 @@
+import junit.framework.Assert;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    @Test
+    public void setNickName()
+    {
+        Player player = new Player();
+
+        int valueReturn0 = player.setNickName("");
+        Assert.assertEquals(-1, valueReturn0);
+
+        int valueReturn1 = player.setNickName("aaaa");
+        Assert.assertEquals(1, valueReturn1);
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,17 @@
 public class Player {
+    private String nickName = null;
+
+    public void Player()
+    {
+        //void
+    }
+
+    public String getNickName()
+    {
+        return nickName;
+    }
+    public int setNickName(String nickName)
+    {
+        return -1;
+    }
 }
