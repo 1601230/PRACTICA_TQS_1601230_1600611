39112e8e905ef640ffa32489dd758acad36ba9fc
1601230 <1601230@uab.cat>
2022-11-06 21:45:57 +0100

    COMMIT 4: Fase Red de Player

--- Test/PlayerTest.java
+++ Test/PlayerTest.java
@@ -11,7 +11,20 @@ public class PlayerTest {
         int valueReturn0 = player.setNickName("");
         Assert.assertEquals(-1, valueReturn0);
 
-        int valueReturn1 = player.setNickName("aaaa");
-        Assert.assertEquals(1, valueReturn1);
+        int valueReturn2 = player.setNickName("    ");
+        Assert.assertEquals(-1, valueReturn2);
+
+        int valueReturn3 = player.setNickName("bb");
+        Assert.assertEquals(-1, valueReturn3);
+
+        int valueReturn4 = player.setNickName("aaaa");
+        Assert.assertEquals(1, valueReturn4);
+
+        int valueReturn5 = player.setNickName("1111");
+        Assert.assertEquals(1, valueReturn5);
+
+        int valueReturn6 = player.setNickName("abababab");
+        Assert.assertEquals(1, valueReturn6);
+
     }
 }
