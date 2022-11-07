4ffc03492c3fc4daf668584ad61ad1210a7f0b80
Joel Angles <1600611@uab.cat>
2022-11-07 16:04:09 +0100

    COMMIT 11: Fase Red de Menu

--- Test/MenuTest.java
+++ Test/MenuTest.java
@@ -1,4 +1,17 @@
+import Controller.Menu;
+import junit.framework.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 public class MenuTest {
+    @Test
+    public void setSelectedOption(){
+        Menu menu = new Menu();
 
+        int valueReturn0 = menu.setSelectedOption(0);
+        Assert.assertEquals(-1, valueReturn0);
+
+        int valueReturn1 = menu.setSelectedOption(1);
+        Assert.assertEquals(1, valueReturn1);
+    }
 }
