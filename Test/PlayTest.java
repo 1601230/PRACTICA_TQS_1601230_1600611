0b3b5d2034f002b488ca1fbc802faf9811420d73
1601230 <1601230@uab.cat>
2022-11-07 14:53:22 +0100

    COMMIT 8: Fase Red de Player

--- Test/PlayTest.java
+++ Test/PlayTest.java
@@ -1,5 +1,17 @@
+import junit.framework.Assert;
+import org.junit.Test;
+import Controller.Play;
 import static org.junit.Assert.*;
 
 public class PlayTest {
+    @Test
+    public void setSelectedLevel() {
+        Play play = new Play();
 
+        int valueReturn0 = play.setSelectedLevel(0);
+        Assert.assertEquals(-1, valueReturn0);
+
+        int valueReturn1 = play.setSelectedLevel(1);
+        Assert.assertEquals(1, valueReturn1);
+    }
 }
--- src/Controller/Play.java
+++ src/Controller/Play.java
@@ -1,4 +1,47 @@
 package Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Play {
+    private List<String> menuLevels = Arrays.asList(new String[]{"1. Easy", "2. Medium", "3. Hard"});
+    private Player player = null;
+    int selectedLevel = 0;
+    private int time = 0;
+    private boolean win = false;
+
+    public void Game()
+    {
+        //void
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+    public int getMenuLevelsSize() { return menuLevels.size(); }
+    public String getElementMenu(int i) { return menuLevels.get(i); }
+    public int getSelectedLevel() { return selectedLevel; }
+    public int getTime()
+    {
+        return time;
+    }
+    public boolean getWin() { return win; }
+    public void setTime(int time)
+    {
+        this.time = time;
+    }
+    public void setWin(boolean b)
+    {
+        this.win = b;
+    }
+    public void setPlayer(Player player)
+    {
+        this.player = player;
+    }
+    public int setSelectedLevel(int selectedLevel)
+    {
+        return -1;
+    }
+
 }
