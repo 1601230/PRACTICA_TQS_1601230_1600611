53198c414baaf9f882e71cdba3c72c5160df0e2d
1601230 <1601230@uab.cat>
2022-11-07 21:50:44 +0100

    COMMIT 22: Fase Green de Board (los metodos ya sirven, podemos pasarlos a la clase board)

--- src/Controller/BoardMock.java
+++ src/Controller/BoardMock.java
@@ -8,14 +8,66 @@ public class BoardMock extends Board{
 
     public int checkCoordinateX(int coordinateX)
     {
-        return -1;
+        if (coordinateX > getBoardRowSize())
+        {
+            System.out.println("ERROR: The row must be a value greater than 1 and less than " + getBoardRowSize() + " inclusive.");
+            return -1;
+        }
+        else if (coordinateX < 1)
+        {
+            System.out.println("ERROR: The row must be a value greater than 1 and less than " + getBoardRowSize() + " inclusive.");
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
     }
     public int checkCoordinateY(int coordinateY)
     {
-        return -1;
+        if (coordinateY > getBoardColumnSize())
+        {
+            System.out.println("ERROR: The column must be a value greater than 1 and less than " + getBoardColumnSize() + " inclusive.");
+            return -1;
+        }
+        else if (coordinateY < 1)
+        {
+            System.out.println("ERROR: The column must be a value greater than 1 and less than " + getBoardColumnSize() + " inclusive.");
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
     }
     public int checkInputMove(int move)
     {
-        return -1;
+        if (move < 1)
+        {
+            System.out.println("ERROR: The selected option must be a value between 1 and " + getPossibleMovesSize() + " .");
+            return -1;
+        }
+        else if (move > getPossibleMovesSize())
+        {
+            System.out.println("ERROR: The selected option must be a value between 1 and " + getPossibleMovesSize() + " .");
+            return -1;
+        }
+        else
+        {
+            if ((move == 2) && (getFlags() == 0))
+            {
+                System.out.println("ERROR: All flags are set, if you want to set another flag you will have to remove some of the flags set.");
+                return -1;
+            }
+            else if ((move == 3) && (getFlags() == getNumberMines()))
+            {
+                System.out.println("ERROR: There is no flag on.");
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
     }
 }
