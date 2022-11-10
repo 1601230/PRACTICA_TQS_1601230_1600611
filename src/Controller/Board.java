089e2d529188741a4965cdf00a7905dc0259e87b
1601230 <1601230@uab.cat>
2022-11-10 10:27:41 +0100

    COMMIT 53: Statement coverage

--- src/Controller/Board.java
+++ src/Controller/Board.java
@@ -326,18 +326,9 @@ public class Board {
 
     public int makeMove(int move, List<Integer> coordinates, int level, View view) //-1: LOSE; 0: CONTINUE; 1: WIN
     {
-        if ((move < 1) || (move > possibleMoves.size()))
-        {
-            return -1;
-        }
-        else if ((coordinates.get(0) < 1) || ((coordinates.get(0) > board.size())) || (coordinates.get(1) < 1) || (coordinates.get(1) > board.get(0).size()))
-        {
-            return -1;
-        }
-        else if ((level < 1) || (level > 3))
-        {
-            return -1;
-        }
+        if ((move < 1) || (move > possibleMoves.size()))  { return -1; }
+        else if ((coordinates.get(0) < 1) || ((coordinates.get(0) > board.size())) || (coordinates.get(1) < 1) || (coordinates.get(1) > board.get(0).size()))  { return -1; }
+        else if ((level < 1) || (level > 3)) { return -1; }
         else
         {
             switch(move)
